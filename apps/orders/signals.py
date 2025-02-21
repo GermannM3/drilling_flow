d833d3c6d3eb48df6fa6ada4d31844191ddf5f97
GermannM <germannm@vk.com>
2025-02-21 08:20:19 +0300

    Обновлен импорт ContractorProfile в apps/orders/signals.py

--- apps/orders/signals.py
+++ apps/orders/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Order
-from contractors.models import ContractorProfile
+from apps.contractors.models import ContractorProfile
 from apps.notifications.utils import send_telegram_notification, send_sms
 
 @receiver(post_save, sender=Order)
