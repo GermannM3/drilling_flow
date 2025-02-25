26ca293aaf33fe1b36b672a43350f1dcf6ed8ff9
GermannM <germannm@vk.com>
2025-02-25 20:22:01 +0300

    Исправлена ошибка с pip3.9 в Vercel

--- vercel-build.sh
+++ vercel-build.sh
@@ -2,12 +2,12 @@
 
 # Выводим версии Python и pip
 echo "Python version:"
-python3 --version || python --version
+python --version
 echo "Pip version:"
-python3 -m pip --version || python -m pip --version
+pip --version
 
 # Устанавливаем зависимости для Vercel
-python3 -m pip install -r requirements-vercel.txt || python -m pip install -r requirements-vercel.txt
+pip install -r requirements-vercel.txt
 
 # Создаем директории для статических файлов
 mkdir -p app/static/webapp/images
